b6942225c900374ee411f82033ce2d16b85ce21d
Ritik Gupta <118038824+ritikgupta1234@users.noreply.github.com>
2022-11-12 18:51:57 +0530

    Middleware n password handling 4(11,12)
    handled the step 4 that is created the hash of the password using bcryptjs and also created a User schema by passing that hashed password in the object

--- authsystem/app.js
+++ authsystem/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 require("./config/database").connect()  //requiring the db and calling connect func
 
-
+const bcrypt = require("bcryptjs")
 const express = require("express")
 const User = require("./model/user")
 const app = express()
@@ -28,8 +28,29 @@ app.post("/register",async(req,res)=>{
     }
     
     //step-4 take care of password
-    // ---------
-
+    //here we will use await bcz it go throughs many algos so it may take time
+    const myEncPassword = await bcrypt.hash(password,10)   //await bcz it is a database operation
+    const user = await User.create({
+        firstname,
+        lastname,
+        email:email.toLowerCase(),
+        password:myEncPassword
+    })
+    
+    //both the above method and below method both are fine you can use anyone while creating
+    // User.create({
+    //     firstname,
+    //     lastname,
+    //     email:email.toLowerCase(),
+    //     password:myEncPassword
+    // })
+    // .then(console.log(`Created successfully`))
+    // .catch(error =>{
+    //     console.log(`Can't create the user`)
+    //     console.log(error)
+    //     process.exit(1)
+    // })
+    
 })
 
 
