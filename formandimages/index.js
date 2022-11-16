7c1113a8ea8fbf4e0a39509290d33ab0c4390c86
Ritik Gupta <118038824+ritikgupta1234@users.noreply.github.com>
2022-11-16 23:00:33 +0530

    completed till 5(6)

--- formandimages/index.js
+++ formandimages/index.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const fileUpload=require("express-fileupload")
 const app=express()
 
 app.set("view engine","ejs")
@@ -6,13 +7,23 @@ app.set("view engine","ejs")
 //midddleware 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/temp/"
+}))
 app.get("/myget",(req,res)=>{
     //postman - {name:ritik}  website - {}
-    // console.log(req.body)
-    // res.send(req.body)
+    console.log(req.body)
+    res.send(req.body)
     //postman - {} website - {name:ritik}
-    console.log(req.query)
-    res.send(req.query)
+    // console.log(req.query)
+    // res.send(req.query)
+})
+
+app.post("/mypost",(req,res)=>{
+    console.log(req.body)
+    console.log(req.files)
+    res.send(req.body)
 })
 
 app.get("/mygetform",(req,res)=>{
