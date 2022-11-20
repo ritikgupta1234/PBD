c558882af6555af4ec72dbff81255660479a72f2
Ritik Gupta <118038824+ritikgupta1234@users.noreply.github.com>
2022-11-20 12:26:48 +0530

    till 8- completed the big promise
    completed the big promise and how to use it also made custom error handler and learned trycatch and async and await

--- Tshirt Store/controllers/homeController.js
+++ Tshirt Store/controllers/homeController.js
@@ -1,13 +1,52 @@
-exports.home = (req,res)=>{
+const BigPromise = require("../middlewares/bigPromise")
+exports.home = BigPromise((req,res)=>{
     res.status(200).json({
         success:true,
         greeting:"Hello from API",
     })
-}
+})
 
-exports.homeDummy = (req,res)=>{
+
+exports.homeDummy = BigPromise((req,res)=>{
     res.status(200).json({
         success:true,
         greeting:"This is another dummy route",
     })
-}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//METHOD-1
+// exports.home = BigPromise((req,res)=>{
+//     res.status(200).json({
+//         success:true,
+//         greeting:"Hello from API",
+//     })
+// })
+
+
+//METHOD-2
+//using async-await and trycatch
+// exports.home = async(req,res)=>{
+//     try {
+//         // const db = await something()
+//         res.status(200).json({
+//             success:true,
+//             greeting:"Hello from API",
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+//feel free to use any of them both are same and will do the same thing and there is third version in which use the promise whenever necessary 
