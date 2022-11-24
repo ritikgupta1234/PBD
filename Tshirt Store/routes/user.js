3be69c66124eed150bab496ecebf0f25cf705cfb
Ritik Gupta <118038824+ritikgupta1234@users.noreply.github.com>
2022-11-24 09:17:08 +0530

    10(3) added logout and email
    added logout controller and email that is nodemailer and mailtrap.io

--- Tshirt Store/routes/user.js
+++ Tshirt Store/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-const {signup,login} = require("../controllers/userContoller")
+const {signup,login,logout} = require("../controllers/userContoller")
 
 router.route("/signup").post(signup)
 router.route("/login").post(login)
+router.route("/logout").get(logout)
 
 module.exports = router
